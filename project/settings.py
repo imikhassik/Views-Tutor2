671bdbafa0c8f8d94eed58f9b566d1698dca939b
Ilya Mikhasik <imikhassik@gmail.com>
2023-02-08 05:11:59 -0500

    Add staticfile_dirs

--- project/settings.py
+++ project/settings.py
@@ -128,3 +128,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILE_DIRS = [
+    BASE_DIR / "static"
+]
