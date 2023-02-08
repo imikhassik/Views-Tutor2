73b173243368244945e7f5b573d7c813f92d3181
Ilya Mikhasik <imikhassik@gmail.com>
2023-02-08 03:39:42 -0500

    Add simpleapp with Product and Category models

---
+++ simpleapp/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.core.validators import MinValueValidator
+
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=50,
+        unique=True,
+    )
+    description = models.TextField()
+    quantity = models.IntegerField(
+        validators=[MinValueValidator(0)],
+    )
+    category = models.ForeignKey(
+        to='Category',
+        on_delete=models.CASCADE,
+        related_name='products',
+    )
+    price = models.FloatField(
+        validators=[MinValueValidator(0.0),]
+    )
+
+    def __str__(self):
+        return f'{self.name.title()}: {self.description[:20]}'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name.title()
