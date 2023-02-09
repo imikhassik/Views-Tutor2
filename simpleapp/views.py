5377c16cc1836aebeaa9b26615e587217dfeea55
Ilya Mikhasik <imikhassik@gmail.com>
2023-02-09 10:19:41 -0500

    Update products template with table, add context to views

--- simpleapp/views.py
+++ simpleapp/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from datetime import datetime
+
 from django.views.generic import ListView, DetailView
 from .models import Product
 
@@ -8,6 +9,12 @@ class ProductsList(ListView):
     ordering = 'name'
     template_name = 'products.html'
     context_object_name = 'products'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['next_sale'] = None
+        return context
 
 
 class ProductDetail(DetailView):
