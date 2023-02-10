bb894f252bcc6f17786c0502ea4931f80274c628
Ilya Mikhasik <imikhassik@gmail.com>
2023-02-10 03:23:59 -0500

    Update currency filter and products template

--- simpleapp/templatetags/custom_filters.py
+++ simpleapp/templatetags/custom_filters.py
@@ -4,6 +4,14 @@ from django import template
 register = template.Library()
 
 
+CURRENCIES_SYMBOLS = {
+    'rub': '₽',
+    'usd': '$',
+}
+
+
 @register.filter()
-def currency(value):
-    return f'{value} ₽'
+def currency(value, code='rub'):
+    postfix = CURRENCIES_SYMBOLS[code]
+
+    return f'{value}{postfix}'
