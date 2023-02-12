0729b19ad9f0d862007c9d3acd1513924bb73adb
Ilya Mikhasik <imikhassik@gmail.com>
2023-02-12 04:01:33 -0500

    Update ProductList view to return filtered list

--- simpleapp/views.py
+++ simpleapp/views.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 from django.views.generic import ListView, DetailView
 from .models import Product
+from .filters import ProductFilter
 
 
 class ProductsList(ListView):
@@ -11,9 +12,15 @@ class ProductsList(ListView):
     context_object_name = 'products'
     paginate_by = 2
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = ProductFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['next_sale'] = 'Распродажа в среду!'
+        context['filterset'] = self.filterset
         return context
 
 
