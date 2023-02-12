a9102414584eebb72b40cbb9096bd02519129bc7
Ilya Mikhasik <imikhassik@gmail.com>
2023-02-12 07:12:35 -0500

    Add url_replace tag

--- simpleapp/templatetags/custom_tags.py
+++ simpleapp/templatetags/custom_tags.py
@@ -9,3 +9,11 @@ register = template.Library()
 @register.simple_tag()
 def current_time(format_string='%b %d %Y'):
     return datetime.utcnow().strftime(format_string)
+
+
+@register.simple_tag(takes_context=True)
+def url_replace(context, **kwargs):
+    d = context['request'].GET.copy()
+    for k, v in kwargs.items():
+        d[k] = v
+    return d.urlencode()
